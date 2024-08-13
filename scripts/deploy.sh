7b651564454198938192e00d249d101ce23478a3
YuvalSal99 <127337423+YuvalSal99@users.noreply.github.com>
2024-08-13 13:04:05 +0300

    Add files via upload

---
+++ scripts/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+docker stop myapp || true
+docker rm myapp || true
+docker pull yuvalsal/final-project:db-46
+docker pull yuvalsal/final-project:node-46
+docker pull yuvalsal/final-project:php-46
+
